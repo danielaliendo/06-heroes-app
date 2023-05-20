a68b4d05357820ad0ed135593d0b09baf5699672
Daniela Liendo <daniela@ualabee.com>
2023-05-20 14:24:07 +0200

    heroes module was created

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeroPageComponent,
+    LayoutPageComponent,
+    ListPageComponent,
+    SearchPageComponent
+  ],
   imports: [
     CommonModule,
     HeroesRoutingModule
