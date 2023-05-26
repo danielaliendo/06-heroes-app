5118e68f602a6a635ea5d5c38bfae3d37e48139f
Daniela Liendo <daniela@ualabee.com>
2023-05-26 10:58:35 +0200

    getHeroById() method was added

--- src/app/heroes/services/hero.service.ts
+++ src/app/heroes/services/hero.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import {catchError, Observable, of} from 'rxjs';
 import { Hero } from '../interfaces/hero';
 import { environment } from 'src/environments/environment';
 
@@ -14,6 +14,13 @@ export class HeroesService {
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
+  }
+
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError( () => of(undefined))
+      )
   }
 
 }
