d246afbc7c47cb6bace93cd23bfdadae3af7b814
Daniela Liendo <daniela@ualabee.com>
2023-05-26 11:42:59 +0200

    getHeroById method was implemented to get current hero by url param id

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HeroesService} from "../../services/hero.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {delay, switchMap} from "rxjs";
+import {Hero} from "../../interfaces/hero";
 
 @Component({
   selector: 'heroes-hero-page',
@@ -6,6 +10,39 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+
+  public hero?: Hero;
+
+  constructor(
+    private heroesService: HeroesService,
+    private activateRoute: ActivatedRoute,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.activateRoute.params
+      .pipe(
+        delay(250),
+        switchMap(({id}) => this.heroesService.getHeroById(id))
+      )
+      .subscribe((hero ): void => {
+
+        if (!hero) {
+          this.router.navigate(['/heroes/list'])
+            .catch(e => console.log(e))
+          return
+        }
+
+        this.hero = hero
+        return
+
+      })
+  }
+
+  goBack():void {
+    this.router.navigateByUrl('heroes/list')
+  }
 
 }
