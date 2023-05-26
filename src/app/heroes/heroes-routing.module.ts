ffd5ca29e4e5e46ace93f001ac45c5cfc9e01751
Daniela Liendo <daniela@ualabee.com>
2023-05-26 10:48:10 +0200

    list path route component was updated for list page component

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -4,6 +4,7 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
 
 const routes: Routes = [
   {
@@ -20,10 +21,10 @@ const routes: Routes = [
       },
       {
         path: 'list',
-        component: NewPageComponent
+        component: ListPageComponent
       },
       {
-        path: 'edit:id',
+        path: 'edit/:id',
         component: NewPageComponent
       },
       {
