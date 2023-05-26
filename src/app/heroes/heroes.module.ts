6d489d073f3c9eb8b1b41f2f43ffcb23c7335760
Daniela Liendo <daniela@ualabee.com>
2023-05-26 10:47:22 +0200

    material module, hero card component and hero image pipe were imported

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
@@ -7,6 +7,9 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
+import { MaterialModule } from '../material/material.module';
+import { HeroCardComponent } from './components/hero-card/hero-card.component';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
 
 @NgModule({
   declarations: [
@@ -14,11 +17,15 @@ import { NewPageComponent } from './pages/new-page/new-page.component';
     LayoutPageComponent,
     ListPageComponent,
     SearchPageComponent,
-    NewPageComponent
+    NewPageComponent,
+    HeroCardComponent,
+    HeroImagePipe
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    MaterialModule,
+    NgOptimizedImage
   ]
 })
 
