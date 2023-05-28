2ec3bbcdb09bb4c5301ebd115b1ada0b6a0dda03
Daniela Liendo <daniela@ualabee.com>
2023-05-28 11:05:11 +0200

    search page component was updated

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {FormControl} from "@angular/forms";
+import {Hero} from "../../interfaces/hero";
+import {HeroesService} from "../../services/hero.service";
+import {MatAutocompleteSelectedEvent} from "@angular/material/autocomplete";
 
 @Component({
   selector: 'heroes-search-page',
@@ -8,5 +12,32 @@ import { Component } from '@angular/core';
 })
 
 export class SearchPageComponent {
+
+  public searchInput = new FormControl('')
+  public heroes: Hero[] = []
+  public selectedHero?: Hero
+
+  constructor(private heroService: HeroesService) {
+  }
+
+  searchHero() {
+    const value:string = this.searchInput.value || ''
+    this.heroService.getSuggestions(value)
+      .subscribe(heroes => this.heroes = heroes)
+  }
+
+  onSelectedOption(event: MatAutocompleteSelectedEvent) {
+    console.log({event})
+
+    if (!event.option.value) {
+      this.selectedHero = undefined
+      return
+    }
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue(hero.superhero);
+    this.selectedHero = hero;
+
+  }
 
 }
