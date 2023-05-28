004e329fc2946c16185493848291b43790d572cd
Daniela Liendo <daniela@ualabee.com>
2023-05-28 13:38:23 +0200

    auth and public guards were implemented in auth and heroes routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
+import { canActivateAuthGuard, canMatchAuthGuard } from "./auth/guards/auth.guard";
+import { canActivatePublicGuard, canMatchPublicGuard } from "./auth/guards/public.guard";
 
 const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canActivate: [canActivatePublicGuard],
+    canMatch: [canMatchPublicGuard]
   },
   {
     path: 'heroes',
-    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule)
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule),
+    canActivate: [canActivateAuthGuard],
+    canMatch: [canMatchAuthGuard]
   },
   {
     path: '404',
